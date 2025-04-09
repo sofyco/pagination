54258d798f12095edba8b92e548ad79dd5df36f0
Dmytro Khaperets <khaperets@gmail.com>
2025-04-09 11:56:00 +0200

    [Core] Removed readonly modifier for DTO

--- src/Query.php
+++ src/Query.php
@@ -8,7 +8,7 @@ use Sofyco\Pagination\Enum\Limit;
  * @property array<string, array<string, string>> $filters
  * @property array<string, string>                $sorting
  */
-final readonly class Query
+final class Query
 {
     public function __construct(public array $filters = [], public array $sorting = [], public int $skip = 0, public int $limit = Limit::DEFAULT)
     {
--- src/Result.php
+++ src/Result.php
@@ -2,7 +2,7 @@
 
 namespace Sofyco\Pagination;
 
-final readonly class Result
+final class Result
 {
     public function __construct(public int $skip, public int $limit, public int $count, public iterable $items)
     {
